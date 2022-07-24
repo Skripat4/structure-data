1e17c683ef1839a9a45a0882358be54b9b628c54
skripat4 <skripat4@gmail.com>
2022-07-24 11:23:26 +0300

    add createMemory

--- (6) - C++/LIST/arrayListInteger.cpp
+++ (6) - C++/LIST/arrayListInteger.cpp
@@ -7,3 +7,16 @@ arrayListInteger::arrayListInteger(){
 arrayListInteger::~arrayListInteger(){
 
 }
+
+void arrayListInteger::createMemory(int size){
+	for (int i = 0; i < size-1; i++){
+		itemListInteger a;
+		a.value = 0;
+		a.next = i + 1;
+		list.push_back(a);
+	}
+	list[0].next = 0;
+	list[size - 1].value = 0;
+	list[size - 1].next = 0;
+	lFree = 1;
+}
