38175c09ef6a23bd400d0fe05117de87e2f5e780
skripat4 <skripat4@gmail.com>
2022-07-27 13:45:53 +0300

    add avl_tree.h

---
+++ (6) - C++/AVL_TREE/avl_tree.h
@@ -0,0 +1,23 @@
+#include<vector>
+
+using namespace std;
+
+struct itemTreeAvlInteger{
+ int value; int next;int last;
+ int parent; int left; int right; unsigned char balance; // avl tree
+};
+
+class avl_tree
+{
+private:
+ vector<itemTreeAvlInteger> tree;
+
+	int lFree;
+	int head;
+	void createMemory(int size);
+public:
+	avl_tree();
+	~avl_tree();
+
+	
+}
