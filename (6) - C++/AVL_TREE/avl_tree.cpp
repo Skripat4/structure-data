baf46fcb7d60645e70fa6da26731ebd982afbea4
skripat4 <skripat4@gmail.com>
2022-07-28 22:59:48 +0300

    fill createMemory

--- (6) - C++/AVL_TREE/avl_tree.cpp
+++ (6) - C++/AVL_TREE/avl_tree.cpp
@@ -8,5 +8,24 @@ avl_tree::~avl_tree(){
 }
 
 void avl_tree::createMemory(int size){
-
+for (int i = 0; i < size; i++){
+		itemTreeAvlInteger a;
+		a.value = 0;
+		a.right = 0 ;
+		a.left = 0;
+		a.balance = 1;
+		a.next = i + 1;
+		a.last = i - 1;
+		tree.push_back(a);
+		
+	}
+	tree[0].next = 0;
+	tree[0].last = 0;
+	tree[size - 1].value = 0;
+	tree[size - 1].next = 0;
+	tree[size - 1].right = 0;
+	tree[size - 1].left = 0;
+	tree[size - 1].balance = 1;
+	lFree = 1;
+	lastFree = 0;
 }
